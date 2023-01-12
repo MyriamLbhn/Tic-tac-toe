26e6661ea7cb7ae8117ff56620b9e1dc88586fbf
MyriamLbhn <lebihan.myriam@gmail.com>
2023-01-12 10:40:38 +0100

    Fonction pour vérifier si le coup est possible

--- tictactoe.py
+++ tictactoe.py
@@ -4,3 +4,7 @@ for i in range(3):
     grille.append([0 for j in range(3)])
 for line in grille:
     print(line)
+ 
+# Fonction pour vérifier si le coup est possible
+def choisir_case(ligne, col, player):
+    grille[ligne][col] = player
