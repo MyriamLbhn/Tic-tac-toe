6830679531a638601218fe05711be814086fb608
MyriamLbhn <lebihan.myriam@gmail.com>
2023-01-12 11:21:33 +0100

    Fonction pour vérifier si un joueur a gagné

--- tictactoe.py
+++ tictactoe.py
@@ -6,5 +6,44 @@ for line in grille:
     print(line)
  
 # Fonction pour vérifier si le coup est possible
-def choisir_case(ligne, col, player):
-    grille[ligne][col] = player
+def choisir_case(ligne, col, joueur):
+    grille[ligne][col] = joueur
+    
+
+# Fonction pour vérifier si le joueur à gagné
+def victoire_jeu(grille, joueur):
+    # Lignes
+    for i in range(3):
+        victoire = True
+        for j in range(3):
+            if grille[i][j] != joueur:
+                victoire = False
+                break
+        if victoire:
+            return victoire
+    # Colonnes
+    for i in range(3):
+        victoire = True
+        for j in range(3):
+            if grille[j][i] != joueur:
+                victoire = False
+                break
+        if victoire:
+            return victoire
+    # Diagonale 1
+    for i in range(3):
+        victoire = True
+        if grille[i][2-i] != joueur:
+            victoire = False
+            break
+        if victoire:
+            return victoire
+    # Diagonale 2
+    for i in range(3):
+        victoire = True
+        if grille[i][i] != joueur:
+            victoire = False
+            break
+        if victoire:
+            return victoire
+    
