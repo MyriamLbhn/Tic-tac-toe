07194ba10dc6aa0fb8cfc55775ac65e3539b1b63
MyriamLbhn <lebihan.myriam@gmail.com>
2023-01-13 09:55:31 +0100

    Ajout des docstrings

--- tictactoe.py
+++ tictactoe.py
@@ -4,19 +4,41 @@ for i in range(3):
     grille.append([0 for j in range(3)])
     
 # Fonction pour aficher la grille :
-def afficher_grille(grille):
+def afficher_grille(grille: list)-> list:
+    """Fonction qui affiche la grille 3x3 du Tic-tac-toe
+
+    Args:
+        grille (list): Une liste de 3 éléments
+
+    Returns:
+        list: Une liste composée de 3 liste, affichée les unes en dessous des autres
+    """
     for line in grille:
         print(line)
  
 # Fonction pour choisir où jouer :
-def choisir_case():
+def choisir_case()-> int:
+    """Fonction qui permet à l'utilisateur de choisir où il souhaite jouer sur la grille
+
+    Returns:
+        int: Indice de la ligne, et de la colonne (1, 2 ou 3)
+    """
     print("Choissisez la ligne puis la colonne sur laquelle vous souhaitez jouer")    
     ligne = eval(input("l= "))
     col = eval(input("c= "))
     return(ligne, col)
     
 # Fonction pour vérifier si un joueur a gagné :
-def est_gagnee(grille, joueur):
+def est_gagnee(grille: list, joueur: int)-> bool:
+    """Fonction qui vérifie sur la partie est gagnée par un des joueurs
+
+    Args:
+        grille (list): la grille du jeu
+        joueur (int): le numéro du joueur (1 ou 2)
+
+    Returns:
+        bool: Retourne "True" en cas de victoire
+    """
     # Lignes
     for ligne in grille:
         if ligne[0]==ligne[1]==ligne[2]==joueur:
@@ -34,7 +56,16 @@ def est_gagnee(grille, joueur):
     return False
     
 # Fonction pour vérifier si le coup est possible 
-def coup_possible(ligne,col):
+def coup_possible(ligne: int,col: int)-> bool:
+    """Fonction qui indique si le coup est possible. C'est-à-dire si la ligne et la colonne choisies par l'utilisateur\n
+    correspond à une case non jouée de la grille.
+
+    Args:
+        ligne (int): Indice de la ligne (1, 2 ou 3)
+        col (int): Indice de la colonne (1, 2 ou 3)
+    Returns:
+        bool: Retourne "True" si le coup est possible.
+    """
     possible = None
     if ligne in (1,2,3) and col in (1,2,3):
         if grille[ligne-1][col-1]==0:
